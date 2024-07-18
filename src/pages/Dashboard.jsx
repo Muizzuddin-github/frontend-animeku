63218349857dc71f74a170eb9e9842085ed71657
unknown <muizzuddin334@gmail.com>
2024-07-18 14:41:12 +0700

    title username

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -116,7 +116,7 @@ const Dashboard = () => {
       <header className="bg-gray-100 ">
         <section className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-20">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
-            Selamat Datang {username}
+            Selamat Datang {username[0].toUpperCase() + username.slice(1)}
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
             Silahkan cari anime yang sudah kami sedikan, dengan daftar anime
